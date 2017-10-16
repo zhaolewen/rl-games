c1cd0d008377eda8f48ee30a0cab73586a137149
Lewen Zhao <zhaolewen@gmail.com>
2017-10-16 21:14:20 +0200

    double DQN

--- space-invader/dqn-space-invader.py
+++ space-invader/dqn-space-invader.py
@@ -27,7 +27,7 @@ def update_target_graph(from_scope, to_scope, tau):
     return op_holder
 
 class QNetwork():
-    def __init__(self,h_size,action_size, reuse=None, img_size=84, learning_rate=0.0001):
+    def __init__(self,h_size,action_size, reuse=None, img_size=84, learning_rate=0.0002):
         self.frame_in = tf.placeholder(tf.float32, [None, img_size * img_size * 3], name="frame_in")
         img_in = tf.reshape(self.frame_in, [-1,img_size, img_size, 3])
 
@@ -72,6 +72,15 @@ class QNetwork():
 
         self.update = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(loss)
 
+        with tf.name_scope("summary"):
+            tf.summary.scalar("loss", loss)
+            tf.summary.scalar("mean_value", tf.reduce_mean(value))
+            tf.summary.scalar("max_advantage", tf.reduce_max(advantage))
+            tf.summary.scalar("mean_target_q", tf.reduce_mean(self.target_q))
+            tf.summary.scalar("mean_pred_q", tf.reduce_mean(self.q_out))
+
+            self.summary_op = tf.summary.merge_all()
+
     def predict_act(self, frame_list, init_state,trace_len=1, batch_size=1, session=None):
         dict = {
             self.frame_in: frame_list,
@@ -83,7 +92,7 @@ class QNetwork():
 
         return act, q_vals,rnn_s
 
-    def update_nn(self, in_frame, target_q_val, acts, trace_len, init_state, batch_size, session):
+    def update_nn(self, in_frame, target_q_val, acts, trace_len, init_state, batch_size, session, writer=None, step=None):
         update_dict = {
             self.frame_in: in_frame,
             self.target_q: target_q_val,
@@ -92,11 +101,13 @@ class QNetwork():
             self.state_init: init_state,
             self.batch_size: batch_size
         }
-        session.run(self.update, feed_dict=update_dict)
+        _, summ = session.run([self.update, self.summary_op], feed_dict=update_dict)
+        if writer is not None and step is not None:
+            writer.add_summary(summ, step)
 
 
 class ExperienceBuffer():
-    def __init__(self, buffer_size=10000):
+    def __init__(self, buffer_size=50000):
         self.buffer = []
         self.buffer_size = buffer_size
 
@@ -113,10 +124,12 @@ class ExperienceBuffer():
             samples_traces.append(episode[pt:pt+trace_len])
 
         samples_traces = np.array(samples_traces)
-        return np.reshape(samples_traces, [batch_size * trace_len, 5]) # why 5 ?
+        return np.reshape(samples_traces, [batch_size * trace_len, 5])
 
-def process_frame(f, height=84,width=84):
+def process_frame(f, height=84,width=84, im_file=None):
     f = scipy.misc.imresize(f, (height, width))
+    if im_file is not None:
+        scipy.misc.imsave(im_file, f)
     return np.reshape(f,[-1])/255.0
 
 def discounted_reward(rs, gamma):
@@ -130,22 +143,26 @@ if __name__=="__main__":
     game_name = 'SpaceInvaders-v0'
     env = gym.make(game_name)
 
-    batch_size = 4 # num of experience traces
-    trace_len = 16
-    update_step = 10
+    batch_size = 16 # num of experience traces
+    trace_len = 8
+    update_step = 5
+    update_target_step = update_step*100
 
-    gamma = 0.999 # discount factor for reward
+    gamma = 0.99 # discount factor for reward
     e_start = 1.0 # prob of random action
     e_end = 0.1
-    annel_steps  = 40000.0 # steps from e_start to e_end
+    annel_steps  = 20000.0 # steps from e_start to e_end
     total_episodes = 10000
 
-    pre_train_steps = 10000 # steps of random action before training begins
+    pre_train_steps = 20000 # steps of random action before training begins
     logdir = "./checkpoints"
+    pic_dir = "./pics/"
 
     h_size = 512
     tau = 0.001
     action_size = env.action_space.n
+    skip_frame = 4
+    save_frame_step = 5000
 
     e_delta = (e_start - e_end) / annel_steps
     exp_buffer = ExperienceBuffer()
@@ -156,12 +173,16 @@ if __name__=="__main__":
     # build graph
     graph = tf.Graph()
     with graph.as_default():
+        global_step = tf.get_variable("global_step",(),tf.int64,tf.zeros_initializer(), trainable=False)
+        inc_global_step = tf.assign(global_step, global_step.value()+1)
+        summ_writer = tf.summary.FileWriter(logdir)
+
         with tf.variable_scope(scope_main):
             main_qn = QNetwork(h_size, action_size)
         with tf.variable_scope(scope_target):
             target_qn = QNetwork(h_size, action_size)
 
-    sv = tf.train.Supervisor(logdir=logdir, graph=graph)
+    sv = tf.train.Supervisor(logdir=logdir, graph=graph, summary_op=None)
     e = e_start
     total_step = 0
 
@@ -179,43 +200,52 @@ if __name__=="__main__":
 
             while True:
                 env.render()
-                # normal process
-                act,_, state_rnn1 = main_qn.predict_act([s_frame], rnn_state, session=sess)
-                act = act[0]
-                if np.random.rand() < e or total_step<pre_train_steps:
-                    act = np.random.randint(0, action_size)
+                if total_step%skip_frame ==0:
+                    s1, reward, done, obs = env.step(last_act)
+                else:
+                    # normal process
+                    act,_, state_rnn1 = main_qn.predict_act([s_frame], rnn_state, session=sess)
+                    act = act[0]
+                    if np.random.rand() < e or total_step<pre_train_steps:
+                        act = np.random.randint(0, action_size)
 
-                last_act = act
+                    last_act = act
 
-                s1, reward, done, obs = env.step(act)
-                s1_frame = process_frame(s1)
+                    s1, reward, done, obs = env.step(act)
+                    if total_step % save_frame_step == 0:
+                        s1_frame = process_frame(s1, im_file=pic_dir+str(total_step))
+                    else:
+                        s1_frame = process_frame(s1)
 
-                ep_buffer.append(np.reshape(np.array([s_frame, act, reward, s1_frame, done]), [1,5]))
+                    ep_buffer.append(np.reshape(np.array([s_frame, act, reward, s1_frame, done]), [1,5]))
 
-                if total_step > pre_train_steps:
-                    if e > e_end:
-                        e -= e_delta
+                    if total_step > pre_train_steps:
+                        if e > e_end:
+                            e -= e_delta
 
-                    if total_step % update_step == 0:
-                        sess.run(update_qn_op)
-                        # update model
-                        state_train = (np.zeros([batch_size, h_size]), np.zeros([batch_size, h_size]))
-                        train_batch = exp_buffer.sample(batch_size, trace_len)
+                        if total_step % update_step == 0:
+                            if total_step % update_target_step==0:
+                                _, step_value = sess.run([update_qn_op, inc_global_step])
+                            else:
+                                step_value = sess.run(inc_global_step)
+                            # update model
+                            state_train = (np.zeros([batch_size, h_size]), np.zeros([batch_size, h_size]))
+                            train_batch = exp_buffer.sample(batch_size, trace_len)
 
-                        pred_act, _, _ = main_qn.predict_act(np.vstack(train_batch[:, 3]),state_train, trace_len, batch_size, sess)
-                        _, q_vals, _ = target_qn.predict_act(np.vstack(train_batch[:, 3]),state_train, trace_len, batch_size, sess)
+                            pred_act, _, _ = main_qn.predict_act(np.vstack(train_batch[:, 3]),state_train, trace_len, batch_size, sess)
+                            _, q_vals, _ = target_qn.predict_act(np.vstack(train_batch[:, 3]),state_train, trace_len, batch_size, sess)
 
-                        end_multiplier = - (train_batch[:, 4] - 1)
-                        double_q = q_vals[range(batch_size * trace_len),pred_act]
-                        target_q_val = train_batch[:, 2] + gamma * double_q * end_multiplier
+                            end_multiplier = - (train_batch[:, 4] - 1)
+                            double_q = q_vals[range(batch_size * trace_len),pred_act]
+                            target_q_val = train_batch[:, 2] + gamma * double_q * end_multiplier
 
-                        in_frames = np.vstack(train_batch[:, 0])
-                        acts = train_batch[:,1]
-                        main_qn.update_nn(in_frames, target_q_val, acts, trace_len, state_train, batch_size, sess)
+                            in_frames = np.vstack(train_batch[:, 0])
+                            acts = train_batch[:,1]
+                            main_qn.update_nn(in_frames, target_q_val, acts, trace_len, state_train, batch_size, sess, summ_writer, step_value)
 
-                s = s1
-                s_frame = s1_frame
-                rnn_state = state_rnn1
+                    s = s1
+                    s_frame = s1_frame
+                    rnn_state = state_rnn1
 
                 ep_rewards.append(reward)
                 total_step += 1
