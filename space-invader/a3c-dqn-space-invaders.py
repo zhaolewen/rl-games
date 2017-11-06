c1ee789abd535daa17e33e536a2b17be17e1c0e3
Lewen Zhao <zhaolewen@gmail.com>
2017-11-06 10:17:20 +0100

    param

--- space-invader/a3c-dqn-space-invaders.py
+++ space-invader/a3c-dqn-space-invaders.py
@@ -332,7 +332,7 @@ if __name__=="__main__":
     action_count = 6
     gamma = 0.99
     #num_workers = multiprocessing.cpu_count() - 2
-    num_workers = 16
+    num_workers = 32
     train_step = 5
     print("Running with {} workers".format(num_workers))
 
